a7bc96966e2d4ac57c4b71a3e3c868bd639bdb76
Euraxluo <euraxluo@outlook.com>
2024-05-31 09:40:54 +0800

    remove export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
         dangerouslyAllowSVG: true,
         domains: ['wrapper.space','localhost'], // 添加你允许加载图片的域名
